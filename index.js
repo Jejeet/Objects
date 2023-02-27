5dc5d1e6dc651aa355304e83af9861c6fcf85878
Adewumi <owonikokoadewunmi@gmail.com>
2023-02-27 06:39:51 +0100

    emumeration

--- index.js
+++ index.js
@@ -42,3 +42,62 @@ const musaJambScores = createJambScores(70, 85, 82, 94);
 // Add the JAMB scores object as a property to the Musa object
 musa.jambScores = musaJambScores;
 console.log(musa);
+
+//4
+Object.assign(),
+
+const person = {
+    name: 'Ade',
+    age: 30,
+    city: 'Ekiti'
+  };
+  
+  // Create a shallow copy of the person object
+  const personCopy = Object.assign({}, person);
+  
+  console.log(personCopy); 
+
+  Spread operator
+
+
+  const person = {
+    name: 'Ade',
+    age: 30,
+    city: 'Ekiti'
+  };
+  
+  // Create a shallow copy of the person object using the spread operator
+  const personCopy = { ...person };
+  
+  console.log(personCopy);
+
+
+  JSON.stringify() and JSON.parse()
+
+  const person = {
+    name: 'Big girl',
+    age: 30,
+    city: 'Canada'
+  };
+  
+  // Create a deep copy of the person object using JSON.stringify() and JSON.parse()
+  const personCopy = JSON.parse(JSON.stringify(person));
+  
+  console.log(personCopy); 
+  
+
+  //5
+
+  const presidentialCandidates = {
+    AAC: 'Omoyele Sowore',
+    ACCORD: 'Christopher Imumolen',
+    APC: 'Bola Ahmed Tinubu',
+    LP: 'Peter Obi',
+    NNPP: 'Rabiu Kwankwaso',
+    PDP: 'Atiku Abubakar',
+ }
+ 
+ for (let party in presidentialCandidates) {
+    console.log(`${presidentialCandidates[party]} is the presidential candidate of ${party}`);
+  }
+  
